192e1b6d0ba97a155a40da6e99f9556b8e83a6ca
Meruyert Rustembek <meruyertrustembek@Meruyerts-MacBook-Air.local>
2025-11-03 21:28:53 -0600

    Step 3: Added pose scoring,  initial score working

--- pilot/apps/mika/bootcamp_app.py
+++ pilot/apps/mika/bootcamp_app.py
@@ -5,9 +5,17 @@ from pyrosetta import *
 init(extra_options="-ignore_unrecognized_res")
 
 parser = argparse.ArgumentParser()
-# Add line here to add an argument
 parser.add_argument("-s", "--structure", required=True, help="Input PDB file")
 args = parser.parse_args()
 
 # Test by printing the filename
 print(f"Input file: {args.structure}")
+
+#Load Pose from file
+mypose = pose_from_pdb(args.structure)
+print(f"Loaded pose with {mypose.total_residue()} residues from: {args.structure}")
+
+# Score the pose
+scorefxn = get_fa_scorefxn()
+initial_score = scorefxn(mypose)
+print(f"Initial score: {initial_score:.2f}")
